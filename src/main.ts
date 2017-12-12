c27cd866c86f4f7265d41a0716cdddffd21e1435
Kenichi Saito <hajime@studiohff.net>
2017-12-13 00:31:35 +0900

    フィルター機能を実装

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,13 @@ import Vue from "vue";
 const app = new Vue({
     el: "#app",
     data: {
-        tweets
+        tweets,
+        search: ""
     },
+    computed: {
+        filteredTweets(): any[] {
+            return this.tweets.filter((t) => t.originalTweet.text.indexOf(this.search) !== -1);
+        }
+    }
 });
 /* tslint:enable:object-literal-sort-keys */
