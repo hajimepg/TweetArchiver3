8e0f65996613d45fd081bf9aaf94ffab299c2bdc
Kenichi Saito <hajime@studiohff.net>
2017-12-13 00:11:29 +0900

    テンプレートをHTMLに追い出し

--- src/main.ts
+++ src/main.ts
@@ -5,27 +5,6 @@ import Vue from "vue";
 /* tslint:disable:object-literal-sort-keys */
 const app = new Vue({
     el: "#app",
-    template: `
-<div class="container">
-    <section class="tweets">
-        <article v-for="tweet in tweets" class="tweet">
-            <div class="icon">
-                <img v-bind:src="tweet.iconFileName" width="48px" height="48px">
-            </div>
-            <div class="name">
-                <strong>{{ tweet.originalTweet.user.name }}</strong>
-                <span>@{{ tweet.originalTweet.user.screen_name }}</span>
-            </div>
-            <div v-html="tweet.autoLinkedText" class="text"></div>
-            <div class="time">{{ tweet.originalTweet.created_at }}</div>
-            <img v-for="m in tweet.media"
-                v-bind:src="m.fileName"
-                v-bind:height="m.displayHeight"
-                v-bind:width="m.displayWidth">
-        </article>
-    </section>
-</div>
-`,
     data: {
         tweets
     },
