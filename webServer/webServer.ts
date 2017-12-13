1b47513216c9f780819ae98738d0886e209f4934
Kenichi Saito <hajime@studiohff.net>
2017-12-14 01:48:02 +0900

    各種ファイルをローカルファイルではなくWebサーバ経由で受信

--- webServer/webServer.ts
+++ webServer/webServer.ts
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
+import * as path from "path";
+
 import * as Koa from "koa";
+import * as KoaStatic from "koa-static";
 
 const app = new Koa();
 
-app.use(async (ctx) => {
-    ctx.body = "Hello World";
-});
+app.use(KoaStatic(path.join(__dirname, "../../html")));
+app.use(KoaStatic(path.join(__dirname, "../../dist")));
 
 app.listen(3000);
