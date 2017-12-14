fb41455706c9e1dc8620b300c3c66270eab0e76e
Kenichi Saito <hajime@studiohff.net>
2017-12-15 00:23:09 +0900

    サーバはAPIアクセスに対しJSON(中身は空)を返す

--- src/server/main.ts
+++ src/server/main.ts
@@ -3,11 +3,20 @@
 import * as path from "path";
 
 import * as Koa from "koa";
+import * as KoaRouter from "koa-router";
 import * as KoaStatic from "koa-static";
 
 const app = new Koa();
 
 app.use(KoaStatic(path.join(__dirname, "../../static")));
 app.use(KoaStatic(path.join(__dirname, "../client")));
+
+const router = new KoaRouter();
+
+router.get("/api/tweets", (ctx, next) => {
+    ctx.body = {};
+});
+
+app.use(router.routes());
 
 app.listen(3000);
